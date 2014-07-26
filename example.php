395a62660b9e4f8368b16a566886840334b0b901
Kyle Rinfreschi <krinfreschi@msn.com>
2014-07-26 20:01:15 +0200

    Added example

---
+++ example.php
@@ -0,0 +1,17 @@
+<?php
+
+use krinfreschi\AsyncStreams\AsyncStreamWrapper;
+
+require_once "vendor/autoload.php";
+
+$resource = fopen('php://temp', 'r+');
+$handle = AsyncStreamWrapper::wrap($resource);
+fwrite($handle, 'data');
+async_stream_register_read($handle, function($handle) {});
+
+async_stream_register_write($handle, function($handle, $written, $unwritten) {
+    fseek($handle, 0);
+    var_dump(stream_get_contents($handle));
+});
+
+AsyncStreamWrapper::run();
